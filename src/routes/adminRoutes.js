aca57fccdee6fe2421ea2c84a5b2df4118178123
TheLastGigolo <gigolobanzai@gmail.com>
2025-05-17 13:40:48 +0700

    Hapus semua file kecuali README.md

--- src/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const { authenticate } = require('../middlewares/authMiddleware');
-
-// Hanya admin yang bisa akses
-router.get('/dashboard', authenticate, adminController.getDashboardData);
-
-module.exports = router;
